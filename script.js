99736b7e97ca29349d7c3b7b6a4b9fabba334ca4
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-11-02 16:41:41 +0530

    2nd commit of domx1

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("heading").style.color = "red";
+document.getElementById("heading").style.backgroundColor = "lightblue";
+document.getElementById("heading").style.border = "solid 3px black";
+document.getElementById("heading").style.borderRadius = "5px";
+document.getElementById("heading").style.justifyContent = "center";
